d3d83fd46bd7cfb23da6e0c2640638c77803b82a
pawel.siwek <pawel.siwek@filament.uk.com>
2021-05-26 22:51:18 +0100

    added greet

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const greet = require('./greet')
-const square = require('./square')
+const greet = require('./greet');
+const square = require('./square');
 
+//greet
+const english = greet.english();
+const polish = greet.polish();
+console.log(english);
+console.log(polish);
+
+//square
 const squareResult = square.square(2);
-const squareFeetResult = square.squareFeet(2)
-const squareClass = square.squareClass
-const resultClass = new squareClass(2)
-
-console.log(resultClass.runAndDisplay())
-
-
-console.log(squareResult)
-console.log(squareFeetResult)
-
+const squareFeetResult = square.squareFeet(2);
+const squareClass = square.squareClass; //undefined
+const resultClass = new squareClass(2); //undefined
+console.log(squareResult);
+console.log(squareFeetResult);
