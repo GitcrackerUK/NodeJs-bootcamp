85ae61e0d477b0c5dd5e8520177b9c8520d02ad5
pawel.siwek <pawel.siwek@filament.uk.com>
2021-05-27 08:55:04 +0100

    initial

--- greet/english.js
+++
@@ -1,7 +0,0 @@
-const greetings = require('./greetings.json')
-
-const greetEN = function () {
-    console.log(greetings.en);
-};
-
-module.exports = greetEN;
--- greet/polish.js
+++
@@ -1,7 +0,0 @@
-const greetings = require('./greetings.json')
-
-function greetPL() {
-    console.log(greetings.pl)
-}
-
-module.exports = greetPL;
