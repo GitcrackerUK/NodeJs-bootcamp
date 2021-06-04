16a159db4674e92f4055b83e1f095243608eafc0
pawel.siwek <pawel.siwek@filament.uk.com>
2021-06-04 07:57:41 +0100

    object and array refresh

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
-const util = require('util')
+// object methods and properties
+
+const object = {
+    greet: 'Hello'
+}
+console.log(object.greet)
+console.log(object['greet'])
+const prop = 'greet'
+console.log(object[prop])
+
+// functions and arrays 
+
+const arr = []
+
+arr.push(function () {
+    console.log('hello world one')
+})
+arr.push(function () {
+    console.log('hello world two')
+})
+arr.push(function () {
+    console.log('hello world three')
+})
+arr.push(function () {
+    console.log('hello world four')
+})
+
+console.log(arr)
+
+arr.forEach(item => {
+    item()
+})
+
+arr.push({ name: 'this is property in the object' })
+
+arr.map(item => {
+    if (item.name) {
+        console.log(item)
+    }
+})
+
 
